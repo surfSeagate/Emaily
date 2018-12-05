194d17f68bdb62b444d7a70585972e8cf4935607
Glenn Curry <surf13jax@gmail.com>
2018-12-05 11:33:56 -0500

    refactor keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 // keys.js - figure out what set of creds to reutun
 
 if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
 }
